2a44889ad2b3c43b8fd25b758c5629f075e62503
chenlicong0821 <chenlicong0821@163.com>
2025-02-19 20:00:44 +0800

    mod url pdf

--- url_pdf_printer.py
+++ url_pdf_printer.py
@@ -25,7 +25,8 @@ def clean_html(html_content):
 
     # 移除不需要的部分，例如广告、导航栏、页脚等
     for element in soup(["header", "footer", "aside", "nav", "script", "style"]):
-        # print("Element:", element)
+        # 打印每个元素的前100个字符
+        # print("Element:", str(element)[:100])
         element.decompose()
 
     return str(soup)
@@ -33,14 +34,24 @@ def clean_html(html_content):
 
 def url_to_pdf(url, output_path):
     try:
-        response = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Upgrade-Insecure-Requests": "1",
+        }
+        response = requests.get(url, headers=headers)
+        # session = requests.Session()
+        # response = session.get(url, headers=headers)
         response.raise_for_status()
-
+        # print("Response:", response.text)
         cleaned_html = clean_html(response.text)
-        # print("Cleaned HTML:\n", cleaned_html)
+        print("Cleaned HTML:\n", cleaned_html[:200])
         # options = {
         #     "no-stop-slow-scripts": None,
-        #     "javascript-delay": 1000,  # 增加延迟时间
+        #     "javascript-delay": 5000,  # 增加延迟时间
         #     "load-error-handling": "ignore",  # 忽略加载错误
         # }
         # 将清理后的HTML内容转换为PDF格式，并保存到指定的输出路径
